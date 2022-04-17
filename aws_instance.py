c3364b1245d36d86c45a7d282fb6655fd8b63bef
Quamruzzaman007 <91254004+Quamruzzaman007@users.noreply.github.com>
2022-04-17 22:41:23 +0530

    Add files via upload
    Added comments

--- aws_instance.py
+++ aws_instance.py
@@ -1,6 +1,6 @@
 import json
 import boto3
-
+# Topic ARN of created topic to send the email
 SNS_ARN = "arn:aws:sns:us-east-1:303840799246:Tag-not-found-in-the-nstances"
 sns = boto3.client("sns")
     
@@ -15,7 +15,7 @@ def lambda_handler(event, context):
             for item in instance.tags:
                 if item['Key'] != 'Name' and item['Key'] != 'Environment' and item['Key'] != 'created_by':
                     instance_tags_not_found.append(instance.id)
-    
+    # ARN of the rule created for every hour
     if 'arn:aws:events:us-east-1:303840799246:rule/every_hour' in event["resources"]:
         if instance_tags_not_found is not None:
             message_contains = "Name or Environment or created_by tag are not in the" + ' , '.join(map(str, instance_tags_not_found))
@@ -25,7 +25,7 @@ def lambda_handler(event, context):
             print(snsresponse)
         else:
             return True
-    
+    # ARN of the rule created for every 6hour
     elif 'arn:aws:events:us-east-1:303840799246:rule/every_6hour' in event["resources"]:
         if instance_tags_not_found is not None:
             message_contains = "Name or Environment or created_by tag are not in the instances so i terminated it"
